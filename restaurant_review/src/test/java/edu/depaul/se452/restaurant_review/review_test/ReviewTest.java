f77c99b80272c5caa478afba389ba05fc898837a
Lu Qi <lqi7@depaul.edu>
2023-04-16 19:29:40 -0500

    add review test

---
+++ restaurant_review/src/test/java/edu/depaul/se452/restaurant_review/review_test/ReviewTest.java
@@ -0,0 +1,21 @@
+package edu.depaul.se452.restaurant_review.review_test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import edu.depaul.se452.restaurant_review.review_lu.Review;
+
+public class ReviewTest {
+    @Test
+    public void testToString() {
+        var review = new Review();
+        review.setUserID(1);
+        review.setRestaurantID(3);
+        review.setContent("lol, this place is good");
+        review.setDate("Apr 14 2023 10:10");
+        review.setStar(4);
+        var expectedNoError = "Review(id=0, userID=1, restaurantID=3, date=Apr 14 2023 10:10, content=lol, this place is good, star=4)";
+        assertEquals(expectedNoError, review.toString());
+    } 
+}
