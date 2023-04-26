9cfec1feb2fe6f72059be3ae03831f3244aea112
Kwasi Appiah-Danquah <83106757+kwasiad@users.noreply.github.com>
2023-04-26 15:48:54 -0500

    Updated Test

--- User_Login/src/test/java/edu/depaul/se452/User_Login/Users Test/UserTest.java
+++ User_Login/src/test/java/edu/depaul/se452/User_Login/Users Test/UserTest.java
@@ -1,4 +1,4 @@
-package edu.depaul.se452.User_Login.Users Test
+package edu.depaul.se452.User_Login.Users Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -19,52 +19,15 @@ public class UsersTest {
     @Transactional
     public void testSaveAndGetUser() {
         // Create a user
-        Users user = new Users();
-        user.setUsername("testuser");
-        user.setPassword("testpassword");
+        Users usr = new Users();
+        usr.setUsername("bobjohnson@example.com");
+        usr.setPassword("password789");
 
         // Save the user to the database
-        entityManager.persist(user);
+        entityManager.persist(usr);
 
         // Get the user from the database
-        Users retrievedUser = entityManager.find(Users.class, user.getId());
-
-        // Check that the retrieved user matches the original user
-        assertNotNull(retrievedUser);
-        assertEquals(user.getUsername(), retrievedUser.getUsername());
-        assertEquals(user.getPassword(), retrievedUser.getPassword());
-    }
-}
-
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-@DataJpaTest
-public class UsersTest {
-    @PersistenceContext
-    private EntityManager entityManager;
-
-    @Test
-    @Transactional
-    public void testSaveAndGetUser() {
-        // Create a user
-        Users user = new Users();
-        user.setUsername("testuser");
-        user.setPassword("testpassword");
-
-        // Save the user to the database
-        entityManager.persist(user);
-
-        // Get the user from the database
-        Users retrievedUser = entityManager.find(Users.class, user.getId());
+        Users retrievedUser = entityManager.find(Users.class, usr.getId());
 
         // Check that the retrieved user matches the original user
         assertNotNull(retrievedUser);
