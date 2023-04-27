d0528c097ac16005801d80428c5731f1a5afd82d
Kwasi Appiah-Danquah <83106757+kwasiad@users.noreply.github.com>
2023-04-27 18:37:19 -0500

    Delete User_Login directory

--- User_Login/src/main/resources/data-h2.sql
+++
@@ -1,5 +0,0 @@
-insert into Users(id, username, password ) values (1, 'johndoe@example.com', 'password123');
-insert into Users(id, username, password ) values (2,'janesmith@example.com', 'password456');
-insert into Users(id, username, password ) values (3,'bobjohnson@example.com', 'password789');
-insert into Users(id, username, password ) values (4,'alicelee@example.com, 'passwordabc');
-
