94cbf08f7dea45945aa691fc8318f360c4817403
Lu Qi <lqi7@depaul.edu>
2023-05-17 00:57:41 -0500

    add package-info for Kwasi and Arshain

---
+++ src/main/java/edu/depaul/se452/restaurant_review/food_arshain/package-info.java
@@ -0,0 +1,3 @@
+package edu.depaul.se452.restaurant_review.food_arshain;
+
+//Arshain is in charge of the 'food' section, this module enables user to do CRUD actions on the foods of resturants
---
+++ src/main/java/edu/depaul/se452/restaurant_review/user_kwasi/package-info.java
@@ -0,0 +1,3 @@
+package edu.depaul.se452.restaurant_review.user_kwasi;
+
+//Kwasi is in charge of the 'user' section, this module enables user to register and login using username and password
